f4970c4b7a37c49469291a17cd1dc7ef443c8079
Mohammad <aali361@gmail.com>
2022-01-21 15:48:55 +0330

    feat: add network models

--- social/network/models.py
+++ social/network/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+from reusable.models import BaseModel
+
+class Network(BaseModel):
+    name = models.CharField(max_length=100)
+    url = models.URLField()
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'({self.pk} - {self.name})'
+
+
+class Publisher(BaseModel):
+    username = models.CharField(max_length=100)
+    network = models.ForeignKey(Network, on_delete=models.CASCADE)
+    is_channel = models.BooleanField(default=False)
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f'({self.pk} - {self.username})'
+
+
+class Post(BaseModel):
+    body = models.CharField(max_length=100)
+    publisher = models.ForeignKey(
+        Publisher, on_delete=models.CASCADE, related_name='posts', null=True
+    )
+    views_count = models.IntegerField(default=0)
+    share_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'({self.pk} - {self.publisher})'
