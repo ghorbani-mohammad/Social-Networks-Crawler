39bb2a174b5d1e8f6f7a625c9ef4d286b2a0204d
Mohammad <aali361@gmail.com>
2022-01-21 15:54:36 +0330

    feat: add network admin

--- social/network/admin.py
+++ social/network/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from reusable.admins import ReadOnlyAdminDateFields
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Network)
+class NetworkAdmin(ReadOnlyAdminDateFields, admin.ModelAdmin):
+    list_display = ("pk", "name", "url", "status")
+
+
+@admin.register(models.Publisher)
+class PublisherAdmin(ReadOnlyAdminDateFields, admin.ModelAdmin):
+    list_display = ("pk", "username", "network", "is_channel", "status", "created_at")
+
+
+@admin.register(models.Post)
+class PostAdmin(ReadOnlyAdminDateFields, admin.ModelAdmin):
+    list_display = ("pk", "publisher", "views_count", "share_count", "created_at")
