800acb6d1438708518665a17b360a5ca18ec52b5
Mohammad <aali361@gmail.com>
2022-01-22 15:48:35 +0330

    feat: add telegram get_code

--- social/telegram/tasks.py
+++ social/telegram/tasks.py
@@ -10,8 +10,8 @@ from . import models
 logger = get_task_logger(__name__)
 
 
-@shared_task(name="telegram")
-def telegram(account_id):
+@shared_task(name="get_code")
+def get_code(account_id):
     account = models.Account.objects.get(pk=account_id)
     client = TelegramClient(
         'telegram_sessions/' + account.phone_number,
@@ -19,12 +19,11 @@ def telegram(account_id):
         settings.TELEGRAM_API_HASH,
     )
 
-    async def test():
-        result = await client.connect()
-        print(result)
-        result = await client.is_user_authorized()
-        print(result)
+    async def main():
+        await client.connect()
+        await client.is_user_authorized()
+        await client.send_code_request(account.phone_number)
 
     loop = asyncio.get_event_loop()
-    task = loop.create_task(test())
+    task = loop.create_task(main())
     loop.run_until_complete(task)
