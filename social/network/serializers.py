bd44f2cc1cc1e5333b0df55f206daf23a3365c7d
Mohammad <aali361@gmail.com>
2022-01-22 09:05:04 +0330

    feat: add Network Viewset

---
+++ social/network/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from . import models
+
+
+class PackageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Network
+        fields = ("id", "name", "url", "status")
--- social/network/views.py
+++ social/network/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import filters
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+from . import models, serializers
+
+
+class ListPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+
+
+class NetworkViewSet(ModelViewSet):
+    queryset = models.Network.objects.order_by("-id")
+    serializer_class = serializers.PackageSerializer
+    pagination_class = ListPagination
+    filter_backends = [
+        filters.SearchFilter,
+        DjangoFilterBackend,
+        filters.OrderingFilter,
+    ]
+    filterset_fields = ["status"]
+    search_fields = ["name"]
+    ordering_fields = ["name"]
