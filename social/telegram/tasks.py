c4b0a87e97d423c2c407336a973d6c4f77958eb7
Mohammad <aali361@gmail.com>
2022-01-22 12:53:08 +0330

    feat: add phone_number to task

--- social/telegram/tasks.py
+++ social/telegram/tasks.py
@@ -4,10 +4,15 @@ from django.conf import settings
 from celery import shared_task
 from celery.utils.log import get_task_logger
 
+from . import models
+
 logger = get_task_logger(__name__)
 
 
 @shared_task(name="telegram")
-def telegram():
-    client = TelegramClient(settings.TELEGRAM_API_ID, settings.TELEGRAM_API_HASH)
+def telegram(account_id):
+    account = models.Account.objects.get(pk=account_id)
+    client = TelegramClient(
+        account.phone_number, settings.TELEGRAM_API_ID, settings.TELEGRAM_API_HASH
+    )
     print(client)
