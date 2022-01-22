821bd5594026f8d00c93a8b7c32a7d46be818a92
Mohammad <aali361@gmail.com>
2022-01-22 12:48:55 +0330

    feat: minor change

--- social/social/settings.py
+++ social/social/settings.py
@@ -86,7 +86,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Celery
-CELERY_BROKER_URL = 'redis://social_redis:6379'
+BROKER_URL = 'redis://social_redis:6379'
 CELERY_RESULT_BACKEND = 'redis://social_redis:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
