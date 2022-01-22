2d95a78576bc194a1a37b5c183dbe0ce6cf79684
Mohammad <aali361@gmail.com>
2022-01-22 12:48:11 +0330

    feat: minor change

--- social/telegram/tasks.py
+++ social/telegram/tasks.py
@@ -1,13 +1,13 @@
 from telethon import TelegramClient
 
 from django.conf import settings
-from celery.decorators import task
+from celery import shared_task
 from celery.utils.log import get_task_logger
 
 logger = get_task_logger(__name__)
 
 
-@task(name="telegram")
+@shared_task(name="telegram")
 def telegram():
     client = TelegramClient(settings.TELEGRAM_API_ID, settings.TELEGRAM_API_HASH)
     print(client)
