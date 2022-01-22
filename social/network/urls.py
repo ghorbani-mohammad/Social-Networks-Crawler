49783839a8faaccdfb977025ab7c98978b7e4233
Mohammad <aali361@gmail.com>
2022-01-22 09:30:22 +0330

    feat: add newtork viewset route

--- social/network/urls.py
+++ social/network/urls.py
@@ -4,6 +4,7 @@ from rest_framework.routers import SimpleRouter
 from . import views
 
 router = SimpleRouter()
+router.register("network", views.NetworkViewSet, basename="network")
 urlpatterns = []
 
 urlpatterns += router.urls
