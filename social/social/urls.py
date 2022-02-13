b519ecc2fe968463c76b596dba7911b46036799b
Mohammad <aali361@gmail.com>
2022-02-14 02:20:11 +0330

    feat: add admin title

--- social/social/urls.py
+++ social/social/urls.py
@@ -5,3 +5,7 @@ urlpatterns = [
     path('secret-admin/', admin.site.urls),
     path("api/v1/soc/", include("network.urls")),
 ]
+
+admin.site.site_header = "Social Administration Panel"
+admin.site.index_title = "Social"
+admin.site.site_title = "Social Admin"
