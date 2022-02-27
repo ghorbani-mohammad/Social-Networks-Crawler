97868abcbb2c40107b19f6d45019b5480cf1d28e
Mohammad <aali361@gmail.com>
2022-02-27 15:59:14 +0330

    fix: minor change

--- social/social/settings.py
+++ social/social/settings.py
@@ -57,13 +57,11 @@ WSGI_APPLICATION = 'social.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        "OPTIONS": {"options": "-c search_path=army"},
         'HOST': 'social_db',
-        "HOST": "postgres",
-        "NAME": "postgres",
-        "USER": env.str("DB_USER"),
-        "PASSWORD": env.str("DB_PASS"),
-        "PORT": env.str("DB_PORT"),
+        'NAME': 'postgres',
+        'PORT': 5432,
+        'USER': env.str('POSTGRES_USER'),
+        'PASSWORD': env.str('POSTGRES_PASSWORD'),
     },
 }
 
