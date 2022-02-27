6c586e624d971c611d7e7e0650f077955b438e09
Mohammad <aali361@gmail.com>
2022-02-27 15:39:16 +0330

    feat: minor change

--- social/social/settings.py
+++ social/social/settings.py
@@ -58,10 +58,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'HOST': 'social_db',
-        'NAME': 'postgres',
-        'PORT': 5432,
-        'USER': env.str('POSTGRES_USER'),
-        'PASSWORD': env.str('POSTGRES_PASSWORD'),
+        "HOST": "postgres",
+        "NAME": "postgres",
+        "USER": env.str("DB_USER"),
+        "PASSWORD": env.str("DB_PASS"),
+        "PORT": env.str("DB_PORT"),
     },
 }
 
