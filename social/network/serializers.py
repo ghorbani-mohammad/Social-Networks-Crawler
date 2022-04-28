014f4016adc577575bc1ab876fa07d3d759b1524
Mohammad <aali361@gmail.com>
2022-04-28 12:05:42 +0430

    feat: minor change

--- social/network/serializers.py
+++ social/network/serializers.py
@@ -64,9 +64,9 @@ class PostSerializer(serializers.ModelSerializer):
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
-        data["category"] = {
-            k: v for k, v in sorted(data["category"].items(), key=lambda item: item[1])
-        }
+        # data["category"] = {
+        #     k: v for k, v in sorted(data["category"].items(), key=lambda item: item[1])
+        # }
         return data
 
     class Meta:
