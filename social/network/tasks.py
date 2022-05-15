86d6dd5bfd08d2b76a6478ea0ac21404cd37fa76
Mohammad <aali361@gmail.com>
2022-05-15 18:44:29 +0430

    fix: minor change

--- social/network/tasks.py
+++ social/network/tasks.py
@@ -124,7 +124,7 @@ def take_backup(backup_id):
             [
                 "ssh",
                 "-i",
-                "secrets/id_rsa_soc_api",
+                "secrets/id_rsa_social_api",
                 "-o",
                 '"StrictHostKeyChecking=no"',
                 "docker exec -t social_db pg_dumpall -c -U postgres | gzip > /root/army/db_backup/social_db_x.sql.gz",
