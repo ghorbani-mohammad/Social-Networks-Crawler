bccb5c83eea14de263cdab5e28d846c5d09b1d89
Mohammad <aali361@gmail.com>
2022-05-21 19:42:57 +0430

    feat: add category-statics to post-count api

--- social/network/utils.py
+++ social/network/utils.py
@@ -9,16 +9,17 @@ from django.db.models.functions import TruncDate, TruncHour, TruncMonth
 from network.models import Network, Channel
 
 KEYWORD_NUMBER = 20
+CATEGORY_NUMBER = 5
 CHANNEL_NUMBER = 5
 
 
 def get_search_modified_qs(apiview, qs):
     for backend in list(apiview.filter_backends):
-        if backend.__name__ != 'SearchFilter':
+        if backend.__name__ != "SearchFilter":
             qs = backend().filter_queryset(apiview.request, qs, apiview)
         else:
-            if 'search' in apiview.request.GET:
-                words = apiview.request.GET['search'].split()
+            if "search" in apiview.request.GET:
+                words = apiview.request.GET["search"].split()
                 query = reduce(or_, [Q(body__contains=word) for word in words])
                 qs = qs.filter(query)
     return qs
@@ -27,7 +28,7 @@ def get_search_modified_qs(apiview, qs):
 def get_search_excluded_qs(apiview):
     qs = apiview.get_queryset()
     for backend in list(apiview.filter_backends):
-        if backend.__name__ != 'SearchFilter':
+        if backend.__name__ != "SearchFilter":
             qs = backend().filter_queryset(apiview.request, qs, apiview)
     return qs
 
@@ -61,7 +62,7 @@ def monthly_iterate(start, finish):
 
 def get_count_statics(qs, search_excluded_qs, type, start=None, end=None):
     result = []
-    if type == 'hourly':
+    if type == "hourly":
         start = start or timezone.localtime()
         end = end or (start - timezone.timedelta(hours=12))
         qs = qs.filter(created_at__gte=start, created_at__lte=end)
@@ -75,25 +76,43 @@ def get_count_statics(qs, search_excluded_qs, type, start=None, end=None):
         s_e_qs = s_e_qs.annotate(hour=TruncHour("created_at"))
         for hour in hourly_iterate(start, end):
             count = qs.filter(hour__hour=hour.hour, hour__day=hour.day).first()
-            count = count['count'] if count else 0
+            count = count["count"] if count else 0
             total_count = s_e_qs.filter(
                 hour__hour=hour.hour, hour__day=hour.day
             ).count()
-            temp_result = {'hour': hour, 'count': count, 'total_count': total_count}
+            temp_result = {"hour": hour, "count": count, "total_count": total_count}
+            cat_query = qs.filter(hour__hour=hour.hour, hour__day=hour.day)
+            cat_query = (
+                cat_query.values("main_category_title")
+                .annotate(count=Count("main_category_title"))
+                .order_by("-count")
+            )
+            categories = []
+            categories_posts = 0
+            for category in cat_query[: min(CATEGORY_NUMBER, len(cat_query))]:
+                categories.append(
+                    {
+                        "category": category["main_category_title"],
+                        "count": category["count"],
+                    }
+                )
+                categories_posts += category["count"]
+            temp_result["categories_posts"] = categories_posts
+            temp_result["categories"] = categories
             networks = {}
             for network in Network.objects.all():
                 networks[network.name] = s_e_qs.filter(
                     hour__hour=hour.hour, hour__day=hour.day, channel__network=network
                 ).count()
-            temp_result['networks'] = networks
+            temp_result["networks"] = networks
             channels = {}
             for channel in Channel.objects.all():
                 channels[channel.name] = s_e_qs.filter(
                     hour__hour=hour.hour, hour__day=hour.day, channel=channel
                 ).count()
-            temp_result['channels'] = channels
+            temp_result["channels"] = channels
             result.append(temp_result)
-    elif type == 'daily':
+    elif type == "daily":
         start = start or timezone.localtime()
         end = end or (start - timezone.timedelta(days=7))
         qs = qs.filter(created_at__gte=start, created_at__lte=end)
@@ -107,15 +126,33 @@ def get_count_statics(qs, search_excluded_qs, type, start=None, end=None):
         s_e_qs = s_e_qs.annotate(day=TruncDate("created_at"))
         for day in daily_iterate(start, end):
             count = qs.filter(day__month=day.month, day__day=day.day).first()
-            count = count['count'] if count else 0
+            count = count["count"] if count else 0
             total_count = s_e_qs.filter(day__month=day.month, day__day=day.day).count()
-            temp_result = {'day': day, 'count': count, 'total_count': total_count}
+            temp_result = {"day": day, "count": count, "total_count": total_count}
+            cat_query = qs.filter(day__month=day.month, day__day=day.day)
+            cat_query = (
+                cat_query.values("main_category_title")
+                .annotate(count=Count("main_category_title"))
+                .order_by("-count")
+            )
+            categories = []
+            categories_posts = 0
+            for category in cat_query[: min(CATEGORY_NUMBER, len(cat_query))]:
+                categories.append(
+                    {
+                        "category": category["main_category_title"],
+                        "count": category["count"],
+                    }
+                )
+                categories_posts += category["count"]
+            temp_result["categories_posts"] = categories_posts
+            temp_result["categories"] = categories
             for network in Network.objects.all():
                 temp_result[network.name] = s_e_qs.filter(
                     day__month=day.month, day__day=day.day, channel__network=network
                 ).count()
             result.append(temp_result)
-    elif type == 'monthly':
+    elif type == "monthly":
         start = start or timezone.localtime()
         end = end or (start - relativedelta(months=7))
         qs = qs.filter(created_at__gte=start, created_at__lte=end)
@@ -129,11 +166,29 @@ def get_count_statics(qs, search_excluded_qs, type, start=None, end=None):
         s_e_qs = s_e_qs.annotate(month=TruncMonth("created_at"))
         for month in monthly_iterate(start, end):
             count = qs.filter(month__year=month.year, month__month=month.month).first()
-            count = count['count'] if count else 0
+            count = count["count"] if count else 0
             total_count = s_e_qs.filter(
                 month__year=month.year, month__month=month.month
             ).count()
-            temp_result = {'month': month, 'count': count, 'total_count': total_count}
+            temp_result = {"month": month, "count": count, "total_count": total_count}
+            cat_query = qs.filter(month__year=month.year, month__month=month.month)
+            cat_query = (
+                cat_query.values("main_category_title")
+                .annotate(count=Count("main_category_title"))
+                .order_by("-count")
+            )
+            categories = []
+            categories_posts = 0
+            for category in cat_query[: min(CATEGORY_NUMBER, len(cat_query))]:
+                categories.append(
+                    {
+                        "category": category["main_category_title"],
+                        "count": category["count"],
+                    }
+                )
+                categories_posts += category["count"]
+            temp_result["categories_posts"] = categories_posts
+            temp_result["categories"] = categories
             for network in Network.objects.all():
                 temp_result[network.name] = s_e_qs.filter(
                     month__year=month.year,
@@ -146,7 +201,7 @@ def get_count_statics(qs, search_excluded_qs, type, start=None, end=None):
 
 def get_keyword_statics(qs, type, start=None, end=None):
     result = []
-    if type == 'hourly':
+    if type == "hourly":
         start = start or timezone.localtime()
         end = end or (start - timezone.timedelta(hours=12))
         qs = qs.filter(created_at__gte=start, created_at__lte=end).annotate(
@@ -155,22 +210,22 @@ def get_keyword_statics(qs, type, start=None, end=None):
         for hour in hourly_iterate(start, end):
             temp = qs.filter(hour__hour=hour.hour, hour__day=hour.day)
             temp = (
-                temp.values('keyword')
-                .annotate(count=Count('keyword'))
-                .order_by('-count')
+                temp.values("keyword")
+                .annotate(count=Count("keyword"))
+                .order_by("-count")
             )
             keywords = []
             for keyword in temp[: min(KEYWORD_NUMBER, len(temp))]:
                 keywords.append(
-                    {'keyword': keyword['keyword'], 'count': keyword['count']}
+                    {"keyword": keyword["keyword"], "count": keyword["count"]}
                 )
             result.append(
                 {
-                    'hour': hour,
-                    'keywords': keywords,
+                    "hour": hour,
+                    "keywords": keywords,
                 }
             )
-    elif type == 'daily':
+    elif type == "daily":
         start = start or timezone.localtime()
         end = end or (start - timezone.timedelta(days=7))
         qs = qs.filter(created_at__gte=start, created_at__lte=end).annotate(
@@ -179,22 +234,22 @@ def get_keyword_statics(qs, type, start=None, end=None):
         for day in daily_iterate(start, end):
             temp = qs.filter(day__month=day.month, day__day=day.day)
             temp = (
-                temp.values('keyword')
-                .annotate(count=Count('keyword'))
-                .order_by('-count')
+                temp.values("keyword")
+                .annotate(count=Count("keyword"))
+                .order_by("-count")
             )
             keywords = []
             for keyword in temp[: min(KEYWORD_NUMBER, len(temp))]:
                 keywords.append(
-                    {'keyword': keyword['keyword'], 'count': keyword['count']}
+                    {"keyword": keyword["keyword"], "count": keyword["count"]}
                 )
             result.append(
                 {
-                    'day': day,
-                    'keywords': keywords,
+                    "day": day,
+                    "keywords": keywords,
                 }
             )
-    elif type == 'monthly':
+    elif type == "monthly":
         start = start or timezone.localtime()
         end = end or (start - relativedelta(months=7))
         qs = qs.filter(created_at__gte=start, created_at__lte=end).annotate(
@@ -203,19 +258,19 @@ def get_keyword_statics(qs, type, start=None, end=None):
         for month in monthly_iterate(start, end):
             temp = qs.filter(month__year=month.year, month__month=month.month)
             temp = (
-                temp.values('keyword')
-                .annotate(count=Count('keyword'))
-                .order_by('-count')
+                temp.values("keyword")
+                .annotate(count=Count("keyword"))
+                .order_by("-count")
             )
             keywords = []
             for keyword in temp[: min(KEYWORD_NUMBER, len(temp))]:
                 keywords.append(
-                    {'keyword': keyword['keyword'], 'count': keyword['count']}
+                    {"keyword": keyword["keyword"], "count": keyword["count"]}
                 )
             result.append(
                 {
-                    'month': month,
-                    'keywords': keywords,
+                    "month": month,
+                    "keywords": keywords,
                 }
             )
     return result
@@ -224,15 +279,15 @@ def get_keyword_statics(qs, type, start=None, end=None):
 def get_channels_statistics(queryset):
     total = 0
     channels = (
-        queryset.values('channel', 'channel__name')
-        .annotate(count=Count('channel'))
-        .order_by('-count')
+        queryset.values("channel", "channel__name")
+        .annotate(count=Count("channel"))
+        .order_by("-count")
     )
     channels = channels[: min(CHANNEL_NUMBER, len(channels))]
     channels = [
-        {'channel': channel['channel__name'], 'count': channel['count']}
+        {"channel": channel["channel__name"], "count": channel["count"]}
         for channel in channels
     ]
     for channel in channels:
-        total += channel['count']
-    return {'channels': channels, 'total': total}
+        total += channel["count"]
+    return {"channels": channels, "total": total}
