eea7da74ed34504ab9dc23228eb7d3e60729c4dd
Mohammad <aali361@gmail.com>
2022-05-28 10:42:25 +0430

    feat: change directory of backup

--- social/network/tasks.py
+++ social/network/tasks.py
@@ -129,7 +129,7 @@ def take_backup(backup_id):
                 "-o",
                 "StrictHostKeyChecking=no",
                 f"root@{settings.SERVER_IP}",
-                f"docker exec -t postgres pg_dumpall -c -U postgres | gzip > /root/army/frontend/dist/postgres_db_{date_time}.sql.gz",
+                f"docker exec -t postgres pg_dumpall -c -U postgres | gzip > /root/army/frontend/dist/backup/postgres_db_{date_time}.sql.gz",
             ]
         )
         backup.link = f"http://{settings.SERVER_IP}/postgres_db_{date_time}.sql.gz"
@@ -142,7 +142,7 @@ def take_backup(backup_id):
                 "-o",
                 "StrictHostKeyChecking=no",
                 f"root@{settings.SERVER_IP}",
-                f"docker exec -t social_db pg_dumpall -c -U postgres | gzip > /root/army/frontend/dist/social_db_{date_time}.sql.gz",
+                f"docker exec -t social_db pg_dumpall -c -U postgres | gzip > /root/army/frontend/dist/backup/social_db_{date_time}.sql.gz",
             ]
         )
         backup.link = f"http://{settings.SERVER_IP}/social_db_{date_time}.sql.gz"
