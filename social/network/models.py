9ef8f48decd60d373ca502a211b6fd76963fb535
Mohammad <aali361@gmail.com>
2022-08-02 00:36:58 +0430

    add ignored field to Keyword

--- social/network/models.py
+++ social/network/models.py
@@ -160,6 +160,7 @@ class Keyword(BaseModel):
         on_delete=models.CASCADE,
     )
     keyword = models.CharField(max_length=100)
+    ignored = models.BooleanField(default=False)
 
     def __str__(self):
         return f"({self.pk} - {self.keyword})"
