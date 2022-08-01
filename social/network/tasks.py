d44c45e4b113af505825fb8584e016948ef2cb49
Mohammad <aali361@gmail.com>
2022-08-01 23:20:28 +0430

    fix: remove-ignored-keywords task

--- social/network/tasks.py
+++ social/network/tasks.py
@@ -194,19 +194,18 @@ def export_channel_list(export_id):
 
 @shared_task()
 def remove_ignored_keywords():
-    ignored_keywords = list(models.IgnoredKeyword.objects.values_list("keyword"))
+    ignored_keywords = list(
+        models.IgnoredKeyword.objects.values_list("keyword", flat=True)
+    )
     first_id = models.Keyword.objects.first().id
     last_id = models.Keyword.objects.last().id
-    batch_size = 1000
+    batch_size = 10000
     current_counter = first_id
     while current_counter < last_id:
-        print(current_counter)
         for item in models.Keyword.objects.filter(
             id__gte=current_counter, id__lte=current_counter + batch_size
         ):
             print(item.keyword in ignored_keywords)
             if item.keyword in ignored_keywords:
-                print("yes")
                 item.delete()
         current_counter += batch_size
-        break
