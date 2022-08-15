40aa79faac177c5f35129e26033d07984260a0d7
Mohammad <aali361@gmail.com>
2022-08-15 23:49:26 +0430

    minor change

--- social/social/urls.py
+++ social/social/urls.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('secret-admin/', admin.site.urls),
+    path("secret-admin/", admin.site.urls),
     path("api/v1/soc/", include("network.urls")),
 ]
 
-admin.site.site_header = "Social Administration Panel"
 admin.site.index_title = "Social"
 admin.site.site_title = "Social Admin"
+admin.site.site_header = "Social Administration Panel"
