edf759d11dcd9e3419fdce8c5b56506547c679cd
Mohammad <aali361@gmail.com>
2022-08-30 23:09:08 +0430

    minor change

--- social/network/tasks.py
+++ social/network/tasks.py
@@ -214,3 +214,7 @@ def remove_blocked_keywords():
             if item.keyword in blocked_keywords:
                 item.delete()
         current_counter += batch_size
+
+@shared_task()
+def import_from_sql_file():
+    pass
