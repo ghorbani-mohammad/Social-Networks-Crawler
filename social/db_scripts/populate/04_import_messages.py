0b1022386faa6eb0ddeed5671379f405f62f4a69
Mohammad <aali361@gmail.com>
2022-08-30 23:29:32 +0430

    add script for import Telegram posts

---
+++ social/db_scripts/populate/04_import_messages.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import random
+from openpyxl import load_workbook
+from django.utils.timezone import make_aware
+
+import django
+
+
+def initial():
+    sys.path.append("../..")
+    os.environ["DJANGO_SETTINGS_MODULE"] = "social.settings"
+    django.setup()
+
+
+initial()
+
+from network.models import Channel, Post
+
+channel_ids = list(
+    Channel.objects.filter(network__name="Telegram").values_list("id", flat=True)
+)
+
+sheet = load_workbook(filename="messages.csv").active
+texts = [cell.value for cell in sheet["C"]]
+dates = [cell.value for cell in sheet["B"]]
+
+for index, text in enumerate(texts):
+    index = index + 1
+    date = dates[index]
+    text = texts[index]
+    post = Post.objects.create(
+        body=text, channel_id=random.choice(channel_ids), imported=True
+    )
+    Post.objects.filter(pk=post.pk).update(
+        created_at=make_aware(date), updated_at=make_aware(date)
+    )
