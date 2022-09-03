d64eba6a9101f329103c2886281f34ddd2129723
Mohammad <aali361@gmail.com>
2022-09-03 21:39:48 +0430

    make terms-level-1&2 blankable

--- social/twitter/models.py
+++ social/twitter/models.py
@@ -7,6 +7,6 @@ class SearchPage(BaseModel):
     name = models.CharField(max_length=100)
     enable = models.BooleanField(default=True)
     message = models.TextField(null=True, blank=True)
-    terms_level_1 = models.TextField(null=True)
-    terms_level_2 = models.TextField(null=True)
+    terms_level_1 = models.TextField(null=True, blank=True)
+    terms_level_2 = models.TextField(null=True, blank=True)
     last_crawl_at = models.DateTimeField(null=True, blank=True)
