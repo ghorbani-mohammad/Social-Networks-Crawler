0c7c9847f88b31cf77d7d32b3dbcfe5dd8a4a65e
Mohammad <aali361@gmail.com>
2022-09-03 10:26:29 +0430

    feat: add SearchPage for Twitter

--- social/twitter/admin.py
+++ social/twitter/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+from reusable.admins import ReadOnlyAdminDateFields
+
+
+@admin.register(models.SearchPage)
+class SearchPageAdmin(ReadOnlyAdminDateFields, admin.ModelAdmin):
+    list_display = ("pk", "name", "enable", "last_crawl_at")
--- social/twitter/models.py
+++ social/twitter/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from reusable.models import BaseModel
 
-# Create your models here.
+
+class SearchPage(BaseModel):
+    url = models.URLField()
+    name = models.CharField(max_length=100)
+    enable = models.BooleanField(default=True)
+    message = models.TextField(null=True, blank=True)
+    last_crawl_at = models.DateTimeField(null=True, blank=True)
