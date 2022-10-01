f7ece5b3ce4fa66fe2a64ae4fbceb800c65dca1b
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-01 19:49:36 +0330

    minor change

--- social/network/apps.py
+++ social/network/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class NetworkConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'network'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "network"
