8fc5cf7cc291789f12c72b9dbc8399f8ec815f9d
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-01 19:51:03 +0330

    minor change

--- social/reusable/admins.py
+++ social/reusable/admins.py
@@ -3,12 +3,12 @@ from django.conf import settings
 
 
 class ReadOnlyAdminDateFields:
-    readonly_fields = ('created_at', 'updated_at', 'deleted_at')
+    readonly_fields = ("created_at", "updated_at", "deleted_at")
 
 
 def url_to_edit_object(obj):
     obj_url = reverse(
-        f'admin:{obj._meta.app_label}_{obj._meta.model_name}_change',
+        f"admin:{obj._meta.app_label}_{obj._meta.model_name}_change",
         args=[obj.id],
     )
-    return f'{settings.BACKEND_URL[:-1]}{obj_url}'
+    return f"{settings.BACKEND_URL[:-1]}{obj_url}"
