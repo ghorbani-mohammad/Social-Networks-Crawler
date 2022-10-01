567f291cc484815b57dc468f0a01b49ba0e97415
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-01 19:52:07 +0330

    minor change

--- social/telegram/apps.py
+++ social/telegram/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class TelegramConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'telegram'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "telegram"
