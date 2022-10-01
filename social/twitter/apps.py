153cafa443064c92aed4e77a97d400db29c169af
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-01 19:52:30 +0330

    minor change

--- social/twitter/apps.py
+++ social/twitter/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class TwitterConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'twitter'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "twitter"
