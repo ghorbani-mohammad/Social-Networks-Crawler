63aedab9e11d611f6374d0bc4df93c53a5b7c1a1
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-07 12:09:12 +0330

    minor change

--- social/linkedin/admin.py
+++ social/linkedin/admin.py
@@ -6,16 +6,11 @@ from reusable.admins import ReadOnlyAdminDateFields
 
 @admin.register(models.JobPage)
 class JobPageAdmin(admin.ModelAdmin):
-    list_display = (
-        "pk",
-        "name",
-        "enable",
-        "last_crawl_at",
-    )
+    list_display = ("pk", "name", "enable", "last_crawl_at")
 
     def crawl_page_action(modeladmin, request, queryset):
         for page in queryset:
             tasks.get_job_page_posts.delay(page.id)
 
     actions = [crawl_page_action]
-    readonly_fields = ReadOnlyAdminDateFields.readonly_fields + ('last_crawl_at',)
+    readonly_fields = ReadOnlyAdminDateFields.readonly_fields + ("last_crawl_at",)
