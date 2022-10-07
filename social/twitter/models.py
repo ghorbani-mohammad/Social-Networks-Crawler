723faf1fd448c6ce5f38e9e67320296f03ade20f
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-07 18:05:51 +0330

    add search-page for SearchPage

--- social/twitter/models.py
+++ social/twitter/models.py
@@ -10,3 +10,6 @@ class SearchPage(BaseModel):
     terms_level_1 = models.TextField(null=True, blank=True)
     terms_level_2 = models.TextField(null=True, blank=True)
     last_crawl_at = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"({self.pk} - {self.name})"
