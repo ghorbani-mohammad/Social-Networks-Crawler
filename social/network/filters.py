269e739478a65e63f4800a93881c60c731ddf6ca
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-08 21:54:04 +0330

    minor change

--- social/network/filters.py
+++ social/network/filters.py
@@ -32,12 +32,7 @@ class PostFilter(FilterSet):
 
     class Meta:
         model = models.Post
-        fields = (
-            "channels",
-            "networks",
-            "date",
-            "tags",
-        )
+        fields = ("channels", "networks", "date", "tags")
 
 
 def keyword_filter_by_channel_ids(queryset, name, value):
@@ -63,12 +58,7 @@ class KeywordFilter(FilterSet):
 
     class Meta:
         model = models.Keyword
-        fields = (
-            "channels",
-            "networks",
-            "date",
-            "tags",
-        )
+        fields = ("channels", "networks", "date", "tags")
 
 
 def channel_filter_by_network_ids(queryset, name, value):
@@ -82,7 +72,4 @@ class ChannelFilter(FilterSet):
 
     class Meta:
         model = models.Channel
-        fields = (
-            "status",
-            "networks",
-        )
+        fields = ("status", "networks")
