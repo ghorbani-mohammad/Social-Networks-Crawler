1c66ef8e40cbfcc60f27ae66fecac4957422b1b4
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-15 19:41:28 +0330

    minor change

--- social/twitter/admin.py
+++ social/twitter/admin.py
@@ -6,11 +6,16 @@ from reusable.admins import ReadOnlyAdminDateFields
 
 @admin.register(models.SearchPage)
 class SearchPageAdmin(admin.ModelAdmin):
-    list_display = ("pk", "name", "enable", "last_crawl_at")
+    list_display = (
+        "pk",
+        "name",
+        "enable",
+        "last_crawl_at",
+    )
 
     def crawl_page_action(modeladmin, request, queryset):
         for page in queryset:
             tasks.crawl_search_page.delay(page.id)
 
     actions = [crawl_page_action]
-    readonly_fields = ReadOnlyAdminDateFields.readonly_fields + ('last_crawl_at',)
+    readonly_fields = ReadOnlyAdminDateFields.readonly_fields + ("last_crawl_at",)
