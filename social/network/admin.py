eddceeceabb419dee1fdd21816bc83a172bf5894
Mohammad <mohammad.ghorbani@hotmail.com>
2022-10-29 20:59:42 +0330

    minor change

--- social/network/admin.py
+++ social/network/admin.py
@@ -141,7 +141,11 @@ class ConfigAdmin(ReadOnlyAdminDateFields, admin.ModelAdmin):
 
 @admin.register(models.ChannelListExport)
 class ChannelListExportAdmin(ReadOnlyAdminDateFields, admin.ModelAdmin):
-    list_display = ("pk", "file", "created_at")
+    list_display = (
+        "pk",
+        "file",
+        "created_at",
+    )
 
 
 @admin.register(models.IgnoredKeyword)
