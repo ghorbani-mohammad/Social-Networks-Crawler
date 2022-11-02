28a49d3907361c2d7bcdef21e2a0092b39935fc3
Mohammad <mohammad.ghorbani@hotmail.com>
2022-11-02 20:38:26 +0330

    minor change

--- social/network/filters.py
+++ social/network/filters.py
@@ -72,7 +72,4 @@ class ChannelFilter(FilterSet):
 
     class Meta:
         model = models.Channel
-        fields = (
-            "status",
-            "networks",
-        )
+        fields = ("status", "networks")
