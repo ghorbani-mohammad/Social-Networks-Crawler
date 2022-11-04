11cfb4171a7b4071e7b99722756f95ce4e14a595
Mohammad <mohammad.ghorbani@hotmail.com>
2022-11-04 09:46:18 +0330

    minor change

--- social/network/serializers.py
+++ social/network/serializers.py
@@ -68,11 +68,7 @@ class ChannelSerializer(serializers.ModelSerializer):
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Tag
-        fields = (
-            "id",
-            "name",
-            "created_at",
-        )
+        fields = ("id", "name", "created_at")
 
 
 class PostSerializer(serializers.ModelSerializer):
