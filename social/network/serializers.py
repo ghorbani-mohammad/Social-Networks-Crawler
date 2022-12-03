137ed9483885cccaa7f905fe7a5bc75eb1fadc32
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-03 17:07:51 +0330

    minor change

--- social/network/serializers.py
+++ social/network/serializers.py
@@ -31,7 +31,14 @@ class ChannelShortSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.Channel
-        fields = ("id", "name", "username", "description", "network", "status")
+        fields = (
+            "id",
+            "name",
+            "username",
+            "description",
+            "network",
+            "status",
+        )
 
 
 class ChannelSerializer(serializers.ModelSerializer):
