01c32b52b7747e7a539032f27956434f023fc50d
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-04 23:34:27 +0330

    minor change

--- social/telegram/admin.py
+++ social/telegram/admin.py
@@ -6,10 +6,5 @@ from reusable.admins import ReadOnlyAdminDateFields
 
 @admin.register(models.Account)
 class AccountAdmin(admin.ModelAdmin):
-    list_display = (
-        "pk",
-        "created_at",
-        "phone_number",
-        "phone_code_hash",
-    )
+    list_display = ("pk", "created_at", "phone_number", "phone_code_hash")
     readonly_fields = ReadOnlyAdminDateFields.readonly_fields + ("phone_code_hash",)
