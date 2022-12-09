39bfe2c2c35cda5a5b05457e17a5cee9c22c9763
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-09 23:57:13 +0330

    minor change

--- social/network/admin.py
+++ social/network/admin.py
@@ -175,7 +175,12 @@ class LogAdmin(admin.ModelAdmin):
         "level",
         "message",
     )
-    list_display = ("pk", "level", "short_message", "time")
+    list_display = (
+        "pk",
+        "level",
+        "short_message",
+        "time",
+    )
 
     def delete_all_logs(modeladmin, request, queryset):
         models.Log.objects.all().delete()
