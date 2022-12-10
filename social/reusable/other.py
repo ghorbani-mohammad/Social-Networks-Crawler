afc172c61921c9fd75999d00ddec226b1c546711
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-10 23:41:55 +0330

    add docstring to only-one-concurrency

--- social/reusable/other.py
+++ social/reusable/other.py
@@ -4,6 +4,14 @@ REDIS_CLIENT = redis.Redis(host="social_redis", port=6379, db=5)
 
 
 def only_one_concurrency(function=None, key="", timeout=None):
+    """This is a decorator that ensure only one function runs on a key at time
+
+    Args:
+        function (_type_, optional): function name. Defaults to None.
+        key (str, optional): key name. Defaults to "".
+        timeout (_type_, optional): lock timeout. Defaults to None.
+    """
+
     def _dec(run_func):
         def _caller(*args, **kwargs):
             have_lock = False
