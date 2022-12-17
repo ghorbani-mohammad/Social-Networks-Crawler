62c0c22a645c503b31b6647590f10aece3c82159
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-17 22:47:34 +0330

    minor improve

--- social/network/admin.py
+++ social/network/admin.py
@@ -67,9 +67,7 @@ class PostAdmin(ReadOnlyAdminDateFields, admin.ModelAdmin):
 
     @admin.display(ordering="created_at", description="created_at")
     def get_created_at(self, instance):
-        return instance.created_at.astimezone(tz("Asia/Tehran")).strftime(
-            "%m/%d %H:%M:%S"
-        )
+        return instance.created_at.strftime("%m/%d %H:%M:%S")
 
 
 @admin.register(models.Keyword)
