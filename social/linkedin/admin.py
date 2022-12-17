1ae3f18bb05360a1bc16913183a460fdf323aed3
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-17 23:56:03 +0330

    fix: fix get-job-page-post arguments

--- social/linkedin/admin.py
+++ social/linkedin/admin.py
@@ -10,7 +10,7 @@ class JobPageAdmin(admin.ModelAdmin):
 
     def crawl_page_action(modeladmin, request, queryset):
         for page in queryset:
-            tasks.get_job_page_posts.delay(page.id)
+            tasks.get_job_page_posts.delay(page.message, page.url)
 
     actions = [crawl_page_action]
     readonly_fields = ReadOnlyAdminDateFields.readonly_fields + ("last_crawl_at",)
