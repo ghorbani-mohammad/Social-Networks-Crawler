5e6d70e5fa104e02c9fbbe98335da4ae08409476
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-18 22:49:44 +0330

    minor change

--- social/network/admin.py
+++ social/network/admin.py
@@ -133,3 +133,8 @@ class LogAdmin(admin.ModelAdmin):
         models.Log.objects.all().delete()
 
     actions = (delete_all_logs,)
+
+
+@admin.register(models.OutputChannel)
+class OutputChannelAdmin(ReadOnlyAdminDateFields, admin.ModelAdmin):
+    list_display = ("pk", "name", "username", "network", "created_at")
