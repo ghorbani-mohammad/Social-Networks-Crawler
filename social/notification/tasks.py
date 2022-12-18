72e05393a46359e9db23be89b8a528e05033e047
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-18 18:07:30 +0330

    add send_message_to_telegram_channel

--- social/notification/tasks.py
+++ social/notification/tasks.py
@@ -1,6 +1,7 @@
 from celery import shared_task
 
 from . import models, utils
+from network.models import OutputChannel
 
 
 @shared_task()
@@ -19,3 +20,23 @@ def send_telegram_message(message):
         resp = utils.telegram_bot_sendtext(bot.telegram_token, account.chat_id, message)
         if not resp["ok"]:
             raise Exception(resp["description"])
+
+
+@shared_task()
+def send_message_to_telegram_channel(message, channel_pk):
+    """This function gets a string message and sends it to specific channel.
+
+    Args:
+        message (str): text message
+        channel_pk (int): id of destination channel (OutputChannel)
+
+    Raises:
+        Exception: if sending message was not successful.
+    """
+    bot = models.TelegramBot.objects.last()
+    channel_output = OutputChannel.objects.get(pk=channel_pk)
+    resp = utils.telegram_bot_sendtext(
+        bot.telegram_token, channel_output.username, message
+    )
+    if not resp["ok"]:
+        raise Exception(resp["description"])
