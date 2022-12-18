be2064af367556f9332876836ef69162744f48eb
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-18 23:11:08 +0330

    add output-channel field to twitter SearchPage

--- social/twitter/models.py
+++ social/twitter/models.py
@@ -10,6 +10,12 @@ class SearchPage(BaseModel):
     terms_level_1 = models.TextField(null=True, blank=True)
     terms_level_2 = models.TextField(null=True, blank=True)
     last_crawl_at = models.DateTimeField(null=True, blank=True)
+    output_channel = models.ForeignKey(
+        "notification.Channel",
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="twitter_pages",
+    )
 
     def __str__(self):
         return f"({self.pk} - {self.name})"
