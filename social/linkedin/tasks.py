674db800c1599b3974966bc239ff417bf8fc38b7
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-18 19:31:25 +0330

    minor change

--- social/linkedin/tasks.py
+++ social/linkedin/tasks.py
@@ -282,8 +282,8 @@ def get_job_page_posts(message, url, output_channel_pk):
         try:
             driver.execute_script("arguments[0].scrollIntoView();", item)
             id = item.get_attribute("data-occludable-job-id")
-            # if DUPLICATE_CHECKER.exists(id):
-            #     continue
+            if DUPLICATE_CHECKER.exists(id):
+                continue
             item.click()
             time.sleep(2)
             job_desc = driver.find_element(By.ID, "job-details").text
