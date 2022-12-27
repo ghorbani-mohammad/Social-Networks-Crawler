9dea6ed712801435ed023e7ccd8a384b8e047f06
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-27 19:38:13 +0330

    refactor: add get-tweet-body function

--- social/twitter/tasks.py
+++ social/twitter/tasks.py
@@ -260,6 +260,13 @@ def get_tweet_id(article):
     )
 
 
+def get_tweet_body(article):
+    return article.find_element(
+        By.XPATH,
+        ".//div[@dir='auto' and starts-with(@id,'id__') and @data-testid='tweetText']",
+    ).text
+
+
 def get_tweet_username(article):
     elements = article.find_elements(
         By.XPATH,
@@ -281,10 +288,7 @@ def get_post_detail_v2(article):
     """
     detail = {}
     detail["id"] = get_tweet_id(article)
-    detail["body"] = article.find_element(
-        By.XPATH,
-        ".//div[@dir='auto' and starts-with(@id,'id__') and @data-testid='tweetText']",
-    ).text
+    detail["body"] = get_tweet_username(article)
     detail["username"] = get_tweet_username(article)
     detail[
         "link"
