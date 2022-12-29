0f996116428190b6b6c0712364b32d27d776d8ff
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-29 19:41:11 +0330

    Add page-link to LinkedIn admin

--- social/linkedin/admin.py
+++ social/linkedin/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from . import models, tasks
+from django.utils.html import format_html
 from reusable.admins import ReadOnlyAdminDateFields
 
 
@@ -9,10 +10,14 @@ class JobPageAdmin(admin.ModelAdmin):
     list_display = (
         "pk",
         "name",
+        "page_link",
         "enable",
         "output_channel",
         "last_crawl_at",
     )
+
+    def page_link(self, obj):
+        return format_html("<a href='{url}'>Link</a>", url=obj.url)
 
     def crawl_page_action(modeladmin, request, queryset):
         for page in queryset:
