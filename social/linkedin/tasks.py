f0b7b5800ca92ae162189277633dd31db5472ba1
Mohammad <mohammad.ghorbani@hotmail.com>
2023-09-28 23:33:36 +0330

    chore(linkedin): adds print message

--- social/linkedin/tasks.py
+++ social/linkedin/tasks.py
@@ -476,6 +476,7 @@ def get_job_company_size(driver):
     except NoSuchElementException:
         return "Cannot-extract-company-size (no such element)"
     except IndexError:
+        print(company_size)
         return "Cannot-extract-company-size (index error)"
 
 
