d2e82aba20531fa41d8a93ced783ca8da30cc852
Mohammad <mohammad.ghorbani@hotmail.com>
2023-12-17 14:42:34 +0330

    feat(linkedin): adds profile field to job-search

--- social/user/models.py
+++ social/user/models.py
@@ -3,7 +3,7 @@ from reusable.models import BaseModel
 
 
 class Profile(BaseModel):
-    user = models.OneToOneField("auth.User", on_delete=models.CASCADE)    
+    user = models.OneToOneField("auth.User", on_delete=models.CASCADE)
     cell_number = models.SlugField(max_length=11, unique=True, null=True, blank=True)
     chat_id = models.CharField(max_length=15, unique=True, null=True, blank=True)
     about_me = models.TextField(null=True, blank=True)
