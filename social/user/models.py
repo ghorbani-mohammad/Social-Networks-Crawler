811084c467bd668798e9a464868d91c774206bfd
Mohammad <mohammad.ghorbani@hotmail.com>
2023-12-17 14:25:04 +0330

    chore(user): adds about-me field

--- social/user/models.py
+++ social/user/models.py
@@ -6,6 +6,7 @@ class Profile(BaseModel):
     user = models.OneToOneField("auth.User", on_delete=models.CASCADE)    
     cell_number = models.SlugField(max_length=11, unique=True, null=True, blank=True)
     chat_id = models.CharField(max_length=15, unique=True, null=True, blank=True)
+    about_me = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return f"({self.pk} - {self.cell_number})"
