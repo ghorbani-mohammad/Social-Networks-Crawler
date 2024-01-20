34546ec10a90cc910d00425ebd914669c75ffffe
Mohammad <mohammad.ghorbani@hotmail.com>
2024-01-20 14:34:06 +0330

    style(ai): code improvements

--- social/ai/admin.py
+++ social/ai/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from . import models
 from reusable.admins import ReadOnlyAdminDateFieldsMIXIN
 
+
 @admin.register(models.CoverLetter)
 class CoverLetterAdmin(ReadOnlyAdminDateFieldsMIXIN, admin.ModelAdmin):
     list_display = ("pk", "profile", "created_at")
